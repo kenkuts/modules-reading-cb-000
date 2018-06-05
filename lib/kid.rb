88a709ad52324e0ca3f8f7d6f040860a0806a3d8
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-06-05 20:39:43 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,11 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  attr_reader :name
+  include Dance
+
+  def initialize(name)
+    @name = name
+  end
+end
